5e93a43b812636d9d5b08025f231229a50d2e720
Ahmed-msayed <ahmed.dev@icloud.com>
2024-05-19 14:15:13 +0300

    Initial Commit

---
+++ DoTask/DoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTaskApp.swift
+//  DoTask
+//
+//  Created by Ahmed Sayed on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
